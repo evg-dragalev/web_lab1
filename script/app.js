d85480296130b8ba5fe3fc9cd099fdb8e0df5b33
Evgeniy Dragalev <evg.dragalev@gmail.com>
2016-10-30 15:35:02 +0300

    Add console error messages, add date format

--- script/app.js
+++ script/app.js
@@ -1,62 +1,73 @@
 'use strict';
 
 const POSTS_MAX_COUNT = 30;
+const SEARCH_WORD = "New York";
 
 $(document).ready(function(){
+
 	var lastPostTime = 0;
 	var responseData = [];
-	var tag = "food";
-	function getTag(){
-		return tag;
-	};
 	var source = $("#photo-entry-template").html();
 
 	var renderingPhotoPosts = function(){
 		var template = Handlebars.compile(source);
-		$('.loaded-info').append(template({objects:responseData}));
-
-		$(".photo-entry").empty();
 		$('.loading').show();
+
 		$.ajax({
-	   		url: 'https://api.vk.com/method/photos.search?q='+tag+'&sort=0&count=20&radius=5000&v=V',
+	   		url: 'https://api.vk.com/method/photos.search?q='+SEARCH_WORD+'&sort=3&count=20&radius=5000&v=V',
 	   		type: 'GET',
 	   		dataType: 'jsonp',
 	   		crossDomain: true,
 	   		success: function(data){
 	   			if (data.error){
-	   				console.log("Error");
-
+	   				console.log("Server return error with code "+data.error.error_code+" : "+data.error.error_msg);
+	   			} else if (data.response[0]==0) {
+	   				console.log("Got empty array from server");
 	   			} else {
 				   		data.response.reverse().forEach( function(entity) {
 						if (entity.pid && lastPostTime < entity.created){
 							responseData.push(entity);
 							lastPostTime = entity.created;
-							entity.createTime = new Date(entity.created*1000);
-							//entity.createTime = prettyDate(new Date(entity.created*1000).toLocaleString());	
+							entity.createTime = (new Date(entity.created*1000)).toLocaleString("ru",{
+								year: 'numeric',
+								month: 'long',
+								day: 'numeric',
+								timezone: 'UTC',
+								hour: 'numeric',
+								minute: 'numeric',
+								second: 'numeric'
+							})
 						}
 						while (responseData.length > POSTS_MAX_COUNT){
 							responseData.pop();
 						}
 					});
-					console.log("Got data on tag "+tag+" refreshing responseDatadata");
+					console.log("Got data on search word "+SEARCH_WORD+" refreshing responseDatadata");
 					console.log(responseData);
 					$('.loading').hide();
 					var html = template(responseData);
+					$(".photo-entry").empty();
 					$('.loaded-info').append(template({objects:responseData.reverse()}));
 					responseData.reverse();
 				}
 			},
-			error: function(){
+			error: function(jqXHR){
+				if (400<=jqXHR && jqXHR < 500){
+					console.log("Error "+jqXHR+" : client error");
+				} else if (500<=jqXHR && jqXHR < 600){
+					console.log("Error "+jqXHR+" : server error");
+				} else {
+					console.log("Error "+jqXHR+" : unknown error");
+				}
 				console.log("Cannot retrieve data from [https://api.vk.com]");
 				$('.loading').hide();
 			}
 		});
 	}
 
-	renderingPhotoPosts(getTag());
-
+	renderingPhotoPosts(SEARCH_WORD);
 	var timer = setTimeout(function repeat(){
-			renderingPhotoPosts(getTag());
+			renderingPhotoPosts(SEARCH_WORD);
 			timer = setTimeout(repeat, 30000);
 		}, 30000);
 })
